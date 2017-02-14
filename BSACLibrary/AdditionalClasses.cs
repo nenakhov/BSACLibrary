d038914acf5f5ed0af0af6964a886376cd7e5633
Dmitry <nenakhov@in.mtis.by>
2017-02-14 21:40:36 +0300

    1

--- BSACLibrary/AdditionalClasses.cs
+++ BSACLibrary/AdditionalClasses.cs
@@ -6,4 +6,14 @@
         public bool isFinded { get; set; }
         public string textCut { get; set; }
     }
+    //Отдельный класс для информации о каждом PDF файле
+    public class pdfDescription
+    {
+        public int id { get; set; }
+        public string publication { get; set; }
+        public bool is_magazine { get; set; }
+        public string date { get; set; }
+        public int issue_number { get; set; }
+        public string file_path { get; set; }
+    }
 }
