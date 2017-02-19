d079d7a0c2fcd7fbb61b4cb26779f6d2ecfde618
nenakhov <nenakhov@in.mtis.by>
2017-02-19 14:12:09 +0300

    up

--- BSACLibrary/QueryExecute.cs
+++ BSACLibrary/QueryExecute.cs
@@ -1,9 +1,5 @@
 ﻿using BSACLibrary.Properties;
 using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
 
 namespace BSACLibrary
@@ -13,49 +9,28 @@ namespace BSACLibrary
         public static void Connect(string query)
         {
             MySqlConnection conn = null;
-            string connStr = "";
-
-            //Формируем строку для подключения
-            connStr = ("server=" + Settings.Default.dbServerIP +
-            ";port=" + Settings.Default.dbServerPort +
-            ";uid=" + Settings.Default.dbUsername +
-            ";pwd=" + Settings.Default.dbPassword +
-            ";");
-            //";database=" + Settings.Default.DBName + ";";
 
             try
             {
-                conn = new MySqlConnection(connStr);
+                conn = new MySqlConnection(Globals.connStr);
+
                 //С помощью этого объекта выполняются запросы к БД
                 MySqlCommand Query = new MySqlCommand();
+
                 //Присваиваем объекту созданное соединение
                 Query.Connection = conn;
+
                 //Устанавливаем соединение с БД
                 conn.Open();
 
-                //Если выбран режим администратора создаем БД и таблицу в ней, если они еще не были созданы
-                if (Settings.Default.isAdmin == true)
-                {
-                    //Создаем базу данных
-                    Query.CommandText = "CREATE DATABASE IF NOT EXISTS " + // Если такая БД уже есть не будет ошибки
-                        "`" + Settings.Default.dbName +
-                        "` CHARACTER SET cp1251 COLLATE cp1251_general_ci;";
-                    //Отправляем запрос
-                    Query.ExecuteNonQuery();
-                }
+                Query.CommandText = query;
+                //Отправляем запрос
+                Query.ExecuteNonQuery();
             }
             //В этом блоке перехватываем возможные ошибки в процессе соединения
             catch (MySqlException ex)
             {
-                //Unable to connect to any of the specified MySQL hosts.
-                if (ex.Message == "Unable to connect to any of the specified MySQL hosts.")
-                {
-                    MessageBox.Show("Проверьте настройки подключения к MySQL", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-                else
-                {
-                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             finally
             {
